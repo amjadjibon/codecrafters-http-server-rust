da795fde58fcd0ceda8f03f36dc47fee52c1e5ab
Amjad Jibon <amjad.jibon@gmail.com>
2024-03-10 02:31:33 +0800

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::net::TcpListener;
+use std::io::{Result, Write};
+use std::net::{TcpListener, TcpStream };
+
+fn handle_client(mut stream: TcpStream) -> Result<()> {
+    stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+}
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -9,6 +14,8 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+
+                handle_client(_stream).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
